fadfcc9b7adb0a5c592e544e74b6bacf109589e4
Mike K <margulo@gmail.com>
2022-09-23 18:36:27 +0100

    add comment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Kazzel!");
 
+//try enumerate
 Enumerable.Range(0, 10).Select(x => x.ToString()).ToList().ForEach(x => Console.WriteLine(x));
+
+
