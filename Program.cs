b0c356149c53614391dbcfc74418d4039fa55a30
Mike K <margulo@gmail.com>
2022-09-23 18:34:39 +0100

    add count print

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Enumerable.Range(0, 10).Select(x => x.ToString()).ToList().ForEach(x => Console.WriteLine(x));
